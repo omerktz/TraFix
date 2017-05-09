0b26f327ed31aa9bc547f13d591cfb3e226c2af3
Omer Katz <omerktz@gmail.com>
2017-05-09 18:12:25 +0300

    increased train batch size

--- train.py
+++ train.py
@@ -4,5 +4,5 @@ import os
 f = sys.argv[1]
 mdir = sys.argv[2]
 m = sys.argv[3]
-nmt.train(saveto=os.path.join(mdir,m), datasets=[f+'.corpus.ll', f+'.corpus.c'], dictionaries=[f+'.vocab.ll.json', f+'.vocab.c.json'], valid_datasets=None, domain_interpolation_indomain_datasets=None,batch_size=80)#, finish_after=10000000)
+nmt.train(saveto=os.path.join(mdir,m), datasets=[f+'.corpus.ll', f+'.corpus.c'], dictionaries=[f+'.vocab.ll.json', f+'.vocab.c.json'], valid_datasets=None, domain_interpolation_indomain_datasets=None,batch_size=400)#, finish_after=10000000)
           
