03165218453807a01c69cd55c60d8898ef673de3
Omer Katz <omerktz@gmail.com>
2017-06-05 14:13:06 +0300

    sync with server

--- external.sh
+++ external.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#THEANO_FLAGS=device=cpu python external.py validate . model.ll-c 10 history
-python external.py validate . model.ll-c 10 history
+THEANO_FLAGS=device=cpu python external.py validate . model.ll-c 10 history
+#python external.py validate . model.ll-c 10 history
