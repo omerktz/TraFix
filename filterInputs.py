436bba86764eb3d2042a733f50eb7936ead5c7d9
Omer Katz <omerktz@gmail.com>
2017-06-08 18:22:37 +0300

    improved filterInputs.py

--- filterInputs.py
+++ filterInputs.py
@@ -1,41 +1,41 @@
-def usage():
-    print 'Usage: python '+sys.argv[0]+' [-d <dataset name>] [-f <tab seperated file name>] <filter script> [<additional filter scripts ...>]'
-    print '\tmust provide either -d or -f but not both'
-    print '\tfilter script should contain a function named filter.\n\tfilter function expects a single string argument and return a boolean'
-    sys.exit(0)
+import argparse
 
-import sys
-if len(sys.argv) <= 3:
-    usage()
+parser = argparse.ArgumentParser(description="Count inputs that match given filters")
+parser.add_argument('-d', '--dataset', type=str, help="filter a dataset file", metavar="DDD", required=False)
+parser.add_argument('-f', '--file', type=str, help="filter a tab seperated file", metavar="FFF", required=False)
+parser.add_argument('-a', '--and', dest='a', help="inputs must match all filters", action='count')
+parser.add_argument('-o', '--or', dest='o', help="inputs need only match one filter", action='count')
+parser.add_argument('filters', help="list of filter files, separated by whitespace", nargs='+')
+args = parser.parse_args()
 
-if '-d' in sys.argv and '-f' in sys.argv:
-    usage()
-if '-d' not in sys.argv and '-f' not in sys.argv:
-    usage()
-if sys.argv.count('-d') > 1 or sys.argv.count('-f') > 1:
-    usage()
+if len(args.filters) > 1:
+    if (1 if args.a else 0) + (1 if args.o else 0) != 1:
+        parser.error('Must choose to either -o or -a in case of multiple filters (but not both)')
 
-if '-d' in sys.argv:
-    flag = '-d'
-    getFilename = lambda x:x+'.corpus.c'
-    cleanLine = lambda l:l.strip()
-if '-f' in sys.argv:
-    flag = '-f'
-    getFilename = lambda x:x
-    cleanLine = lambda l:l.strip().split('\t')[0].strip()
+if (1 if args.dataset else 0) + (1 if args.file else 0) != 1:
+    parser.error('Must choose to either a dataset (-d) or a file (-f) as input (but not both)')
 
 def load_filter(f):
     if f.endswith('.py'):
         f = f[:-3]
     return __import__(f).filter
 
-index = sys.argv.index(flag)
-filters = map(load_filter,sys.argv[1:index]+sys.argv[index+2:])
-with open(getFilename(sys.argv[index+1]),'r') as fin:
+print args
+
+filters = map(load_filter,args.filters)
+(filename,cleanLine) = (args.dataset+'.corpus.c', lambda l:l.strip()) if args.dataset else (args.file,lambda l:l.strip().split('\t')[0].strip())
+with open(filename,'r') as fin:
     count = 0
+    total =0
     for l in fin.readlines():
         l = cleanLine(l)
-        if len(filter(lambda f: not f(l),filters)) == 0:
-            count += 1
-    print str(count)+' lines matched filter(s)'
+        if len(l) > 0:
+            total += 1
+            if args.a:
+                if len(filter(lambda f: not f(l),filters)) == 0:
+                    count += 1
+            else:
+                if len(filter(lambda f: f(l), filters)) > 0:
+                    count += 1
+    print str(count)+'/'+str(total)+' lines matched filter(s)'
 
