7e81552024c82f15d7f65343f599bf5a97d0abe3
Omer Katz <omerktz@gmail.com>
2017-07-12 21:08:28 +0300

    fixed bug in relevance tagging

--- dynet/seqTagger/tagRelevance.py
+++ dynet/seqTagger/tagRelevance.py
@@ -18,27 +18,31 @@ with open(sys.argv[1]+'.words','r') as fin:
         for l in fin.readlines():
             instructions = map(lambda x: x.strip() +' ;', filter(lambda y: len(y.strip())>0, l.split(';')))
             temps = {}
+            lengths = {}
             dependencies = {}
             counter = 0
             finals = {}
+            index = 0
             for inst in instructions:
-                dependencies[inst] = []
+            	lengths[index] = inst.count(' ')+1
+            	dependencies[index] = []
                 parts = inst.split(' ')
                 for p in parts:
                     if re.match('%[0-9]+',p):
-                        dependencies[inst].append(p)
+                        dependencies[index].append(p)
                 if re.match('%[0-9]+',parts[0]):
-                    temps[parts[0]] = inst
+                    temps[parts[0]] = index
                 if parts[0] == 'store':
-                        finals[inst] = counter
+                        finals[index] = counter
                         if '%Y' in parts:
                             counter += 1
+                index += 1
             relevance = {}
-            for inst in instructions:
+            for inst in range(index):
                 relevance[inst] = set()
             for inst in finals.keys():
                 propogateRelevance(inst, dependencies, temps, finals[inst], relevance)
             res = ''
-            for inst in instructions:
-                res += ' '+' '.join(map(lambda i:str(sorted(list(relevance[inst]))).replace(' ',''), range(inst.count(' ')+1)))
+            for inst in range(index):
+                res += ' '+' '.join(map(lambda i:str(sorted(list(relevance[inst]))).replace(' ',''), range(lengths[inst])))
             fout.write(res.strip()+'\n')
