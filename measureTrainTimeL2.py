982f27dcab1e46418cb67141ab7efad59efe7a0e
Omer Katz <omerktz@gmail.com>
2017-11-09 23:08:42 +0200

    styling/formatting

--- measureTrainTimeL2.py
+++ measureTrainTimeL2.py
@@ -40,8 +40,13 @@ results = pool.map(run, product(range(10000, 0, -1000), range(100, 0, -10)))
 pool.close()
 pool.join()
 import csv
+
 with open('resultsL2.csv', 'w') as f:
 	csvf = csv.writer(f)
-	csvf.writerow(['MaxNum', 'TrainDatsetSize', 'TrainTime', 'TotalEpochs', 'BestEpoch', 'BestDevBleu', 'TranslationTime', 'TestBleu', 'Successes', 'UnParsable', 'Failures'])
+	csvf.writerow(
+		['MaxNum', 'TrainDatsetSize', 'TrainTime', 'TotalEpochs', 'BestEpoch', 'BestDevBleu', 'TranslationTime',
+		 'TestBleu', 'Successes', 'UnParsable', 'Failures'])
 	for r in results:
-		csvf.writerow([results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8], results[9], results[10]])
+		csvf.writerow(
+			[results[0], results[1], results[2], results[3], results[4], results[5], results[6], results[7], results[8],
+			 results[9], results[10]])
