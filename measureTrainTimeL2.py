e66bf8d0e7dc7dc10c2c9f07855587e2cafed0cf
Omer Katz <omerktz@gmail.com>
2017-11-11 14:01:10 +0200

    fix bugs in measureTrainTime.py

--- measureTrainTimeL2.py
+++ measureTrainTimeL2.py
@@ -1,42 +1,48 @@
 from multiprocessing import Pool
-import os
+from subprocess import Popen
 from itertools import product
 import time
+import os
 from evaluate import evaluate
 
 
 def run((nums, length)):
-	train_start = time.time()
-	os.system(
-		'(time python api_dynmt.py datasets/train_{0}_{1} datasets/validate_{0}_{1} datasets/test_{0}_{1} -m models/model_L2_{0}_{1} -po -c configs/dynmtL2.config --train --cleanup) &> outputs/dynmt_train_L2_{0}_{1}.out'.format(
-			nums, length))
-	train_end = time.time()
+	with open('outputs/dynmt_train_L2_{0}_{1}.out'.format(nums, length),'w') as f:
+		train_start = time.time()
+		Popen(
+			'time python api_dynmt.py datasets/train_{0}_{1} datasets/validate_{0}_{1} datasets/test_{0}_{1} -m models/model_L2_{0}_{1} -po -c configs/dynmtL2.config --train --cleanup'.format(
+				nums, length).split(' '), stdout=f, stderr=f).wait()
+		train_end = time.time()
 	with open('outputs/dynmt_train_L2_{0}_{1}.out'.format(nums, length), 'r') as f:
 		lines = f.readlines()
-	epochs = int(filter(lambda x: x.startswith('last epoch is '), lines).strip()[14:])
-	dev_blue_line = filter(lambda x: x.startswith('epoch: ' + str(epochs)), lines)
+	epochs = int(filter(lambda x: x.startswith('last epoch is '), lines)[0].strip()[14:])
+	dev_blue_line = filter(lambda x: x.startswith('epoch: ' + str(epochs)), lines)[0]
 	dev_blue_line = dev_blue_line[dev_blue_line.index('best dev bleu ') + 14:]
 	dev_bleu = float(dev_blue_line[:dev_blue_line.index(' ')])
 	dev_blue_line = dev_blue_line[dev_blue_line.index('(epoch ') + 7:]
 	best_epoch = int(dev_blue_line[:dev_blue_line.index(')')])
-	translate_start = time.time()
-	os.system(
-		'(time python api_dynmt.py datasets/train_{0}_{1} datasets/validate_{0}_{1} datasets/test_{0}_{1} -m models/model_L2_{0}_{1} -po -c configs/dynmtL2.config --translate --cleanup) &> outputs/dynmt_translate_L2_{0}_{1}.out'.format(
-			nums, length))
-	translate_end = time.time()
+	os.system('mv datasets/test_{0}_{1}.corpus.5.out datasets/test_{0}_{1}.corpus.5.out.old'.format(nums, length))
+	with open('outputs/dynmt_translate_L2_{0}_{1}.out'.format(nums, length), 'w') as f:
+		translate_start = time.time()
+		Popen(
+			'time python api_dynmt.py datasets/train_{0}_{1} datasets/validate_{0}_{1} datasets/test_{0}_{1} -m models/model_L2_{0}_{1} -po -c configs/dynmtL2.config --translate --cleanup'.format(
+				nums, length).split(' '), stdout=f, stderr=f).wait()
+		translate_end = time.time()
 	with open('outputs/dynmt_translate_L2_{0}_{1}.out'.format(nums, length), 'r') as f:
 		lines = f.readlines()
-	test_blue = float(filter(lambda x: x.startswith('test bleu: '), lines).strip()[11:-1])
+	test_blue = float(filter(lambda x: x.startswith('test bleu: '), lines)[0].strip()[11:-1])
 	with open('datasets/test_{0}_{1}.corpus.c'.format(nums, length), 'r') as fc:
 		with open('datasets/test_{0}_{1}.corpus.ll'.format(nums, length), 'r') as fll:
 			with open('datasets/test_{0}_{1}.corpus.5.out'.format(nums, length), 'r') as fout:
 				(nidentical, nsuccess, nparse, nfail, ntimeout) = evaluate(5, fc, fll, fout, True, True)
+	os.system('mv datasets/test_{0}_{1}.corpus.5.out datasets/test_{0}_{1}.corpus.5.L2.out'.format(nums, length))
+	os.system('mv datasets/test_{0}_{1}.corpus.5.out.old datasets/test_{0}_{1}.corpus.5.out'.format(nums, length))
 	return (nums, length, train_end - train_start, epochs, best_epoch, dev_bleu,
 			translate_end - translate_start, test_blue, nidentical + nsuccess, nparse, nfail + ntimeout)
 
 
 pool = Pool(processes=32)
-results = pool.map(run, product(range(10000, 0, -1000), range(100, 0, -10)))
+results = pool.map(run, product(range(100, 0, -10), range(10000, 0, -1000)))
 pool.close()
 pool.join()
 import csv
