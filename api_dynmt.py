b85c09f6f756ad43c07bdb67f09aa9b9fd3d68f4
Omer Katz <omerktz@gmail.com>
2017-12-20 14:36:42 +0200

    default epoch limit

--- api_dynmt.py
+++ api_dynmt.py
@@ -14,7 +14,7 @@ def main(args):
 								  '{2}.corpus.ll {2}.corpus.{3} {4} {17}.ll {17}.{3} --epochs={5} --batch-size={6} --eval-after={7} ' \
 								  '--max-len={8} --max-pred={9} --max-patience={10} --beam-size={11} --plot ' \
 								  '--lstm-layers={12} --models-to-save={13} {14} {15} {16}' \
-		.format(train, validation, test, ext, model, config.getint('DyNmt', 'epochs'),
+		.format(train, validation, test, ext, model, args.epochs if args.epochs else config.getint('DyNmt', 'epochs'),
 				config.getint('DyNmt', 'batch_size'), config.getint('DyNmt', 'eval_after'),
 				config.getint('DyNmt', 'max_len'), config.getint('DyNmt', 'max_pred'),
 				config.getint('DyNmt', 'max_patience'), 1 if args['train'] else args['num_translations'],
@@ -71,6 +71,7 @@ if __name__ == "__main__":
 	parser.add_argument('--cleanup', help="remove all intermediate files after training", action='count')
 	parser.add_argument('--silent', help="hide all output", action='count')
 	parser.add_argument('-s', '--seed', type=int, help="random seed")
+	parser.add_argument('-e', '--epochs', type=int, help="number of epochs to train")
 	args = parser.parse_args()
 
 	if (args.train and args.translate) or not (args.train or args.translate):
