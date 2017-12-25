b0d2683308beb81cd2dcfb169f07572372e865e9
Omer Katz <omerktz@gmail.com>
2017-12-25 13:22:18 +0200

    initial active learner code

---
+++ activeLearner.py
@@ -0,0 +1,200 @@
+import os
+from subprocess import Popen, check_output
+import random
+import csv
+
+
+class ActiveLearner:
+	def __init__(self, input, output_dir, codenator_config='configs/codenator4active.config',
+				 dynmt_config='configs/dynmt4active.config', num_translations=5, success_percentage=0.99,
+				 validation_size=1000, train_size_initial=0, train_size_increment=1000):
+		# store parameters
+		self.input = input
+		self.output_dir = output_dir
+		self.codenator_config = codenator_config
+		self.dynmt_config = dynmt_config
+		self.num_translations = num_translations
+		self.success_percentage = success_percentage
+		self.validation_size = validation_size
+		self.train_size_initial = train_size_initial
+		self.train_size_increment = train_size_increment
+		# set external scripts paths
+		self.codenator = 'codenator.py'
+		self.api_dynmt = 'api_dynmt.py'
+		self.evaluate = 'evaluate4active.py'
+		# set work paths
+		self.datasets_path = os.path.join(self.output_dir, 'datasets')
+		self.models_path = os.path.join(self.output_dir, 'models')
+		self.outputs_path = os.path.join(self.output_dir, 'outputs')
+		self.vocab_path = os.path.join(self.output_dir, 'vocab')
+		# initialize
+		self.initialize_datasets()
+
+	def initialize_datasets(self):
+		# clear output dir
+		if os.path.exists(self.output_dir):
+			if os.path.isdir(self.output_dir):
+				import shutil
+				shutil.rmtree(self.output_dir)
+			else:
+				os.remove(self.output_dir)
+		os.makedirs(self.output_dir)
+		os.system('cp {0} {1}'.format(self.codenator_config,
+									  os.path.join(self.output_dir, os.path.basename(self.codenator_config))))
+		os.system(
+			'cp {0} {1}'.format(self.dynmt_config, os.path.join(self.output_dir, os.path.basename(self.dynmt_config))))
+		# update config paths
+		self.codenator_config = os.path.join(self.output_dir, os.path.basename(self.codenator_config))
+		self.dynmt_config = os.path.join(self.output_dir, os.path.basename(self.dynmt_config))
+		# generate vocabularies
+		os.system('python {0} -po --vocabs -o {1} -c {2}'.format(self.codenatorself.vocab_path, self.codenator_config))
+		# create work directories
+		os.makedirs(self.models_path)
+		os.makedirs(self.outputs_path)
+		os.makedirs(self.datasets_path)
+		# create initial datasets
+		os.system('cp {0} {1}'.format(args.input, os.path.join(self.datasets_path, 'test0')))
+		self.initial_test_size = int(check_output('cat {0} | wc -l'.format(args.input)).strip())
+		os.system(
+			'python {0} -po -o {1} -c {2} -n {3}'.format(self.codenator, os.path.join(self.datasets_path, 'validate0'),
+														 self.codenator_config, args.validation_size))
+		os.system(
+			'python {0} -po -o {1} -c {2} -n {3}'.format(self.codenator, os.path.join(self.datasets_path, 'train0'),
+														 self.codenator_config, args.train_size_initial))
+
+	# train model until no more progress is made on validation set and translate test set
+	def train_and_translate(self, i, epochs=None):
+		# train
+		with open(os.path.join(self.outputs_path, 'train%d' % i), 'w') as f:
+			Popen('python {0} {1} {2} {3} {4} -m {5} -po -c {6} --train{7}'.format(self.api_dynmt,
+																				   os.path.join(self.datasets_path,
+																								'train%d' % i),
+																				   os.path.join(self.datasets_path,
+																								'validate%d' % i),
+																				   os.path.join(self.datasets_path,
+																								'test%d' % i),
+																				   self.vocab_path,
+																				   os.path.join(self.models_path,
+																								'model%d' % i),
+																				   args.dynmt_config,
+																				   '' if (epochs is None) else (
+																						   ' -e ' + str(epochs))).split(
+				' '), stdout=f, stderr=f).wait()
+		# translate
+		with open(os.path.join(self.outputs_path, 'translate%d' % i), 'w') as f:
+			Popen('python {0} {1} {2} {3} {4} -m {5} -po -c {6} --translate -n {7}'.format(self.api_dynmt, os.path.join(
+				self.datasets_path, 'train%d' % i), os.path.join(self.datasets_path, 'validate%d' % i), os.path.join(
+				self.datasets_path, 'test%d' % i), self.vocab_path, os.path.join(self.models_path, 'model%d' % i),
+																						   args.dynmt_config,
+																						   args.num_translations).split(
+				' '), stdout=f, stderr=f).wait()
+
+	# generate new datasets and combine with previous set of datasets
+	def update_datasets(self, i):
+
+		# combine two dataset to a single dataset and shuffle it
+		# if limit is not None, keep only the top <limit> entries
+		def combine_dataset(new_dataset, old_dataset, limit=None):
+			datasets = {}
+			for ext in ['ll', 'c', 'po']:
+				datasets[ext] = []
+				with open('{0}.{1}'.format(new_dataset, ext), 'r') as f:
+					datasets[ext] += map(lambda x: x.strip(), f.readlines())
+				with open('{0}.{1}'.format(old_dataset, ext), 'r') as f:
+					datasets[ext] += map(lambda x: x.strip(), f.readlines())
+			indexes = random.shuffle(range(len(datasets.values()[0])))
+			if limit:
+				limit = min(limit, len(indexes))
+				indexes = indexes[0: limit]
+			for ext in ['ll', 'c', 'po']:
+				with open('{0}.{1}'.format(new_dataset, ext), 'w') as f:
+					f.write('\n'.join([datasets[ext][i] for i in indexes] + ['']))
+
+		os.system(
+			'python {0} -po -o {1} -c {2} -n {3}'.format(self.codenator,
+														 os.path.join(self.datasets_path, 'validate%d' % i),
+														 self.codenator_config, args.validation_size))
+		combine_dataset(os.path.join(self.datasets_path, 'validate%d' % i),
+						os.path.join(self.datasets_path, 'validate%d' % (i - 1)), limit=args.validation_size)
+		os.system('python {0} -po -o {1} -c {2} -n {3}'.format(self.codenator,
+															   os.path.join(self.datasets_path, 'train%d' % i),
+															   self.codenator_config, args.train_size_increment))
+		combine_dataset(os.path.join(self.datasets_path, 'train%d' % i),
+						os.path.join(self.datasets_path, 'train%d' % (i - 1)))
+
+	# return True if successfully translated *enough* entries
+	def results_sufficient(self, i):
+
+		# update test dataset keeping only unsolved entries
+		def update_testset(i):
+			num_remaining = 0
+			with open(os.path.join(self.datasets_path, 'test%d.c' % (i + 1)), 'w') as fc:
+				with open(os.path.join(self.datasets_path, 'test%d.po' % (i + 1)), 'w') as fpo:
+					with open(os.path.join(self.datasets_path, 'test%d.ll' % (i + 1)), 'w') as fll:
+						with open(os.path.join(self.datasets_path, 'test%d.fail.%d.csv' % (i, args.num_translations)),
+								  'r') as fin:
+							for l in list(csv.reader(fin))[1:]:
+								fc.write(l[1] + '\n')
+								fpo.write(l[2] + '\n')
+								fll.write(l[3] + '\n')
+								num_remaining += 1
+			with open(os.path.join(self.output_dir, 'successes'), 'a') as fout:
+				csvout = csv.writer(fout)
+				with open(os.path.join(self.datasets_path, 'test%d.success.%d.csv' % (i, args.num_translations)),
+						  'r') as fin:
+					for l in list(csv.reader(fin))[1:]:
+						csvout.writerow(l[1:])
+			return num_remaining
+
+		with open(os.path.join(self.outputs_path, 'evaluate%d' % i), 'w') as f:
+			Popen('python {0} {1} {2} -po --convert --llvm'.format(self.evaluate,
+																   os.path.join(self.datasets_path, 'test%d' % i),
+																   args.num_translations).split(' '), stdout=f,
+				  stderr=f).wait()
+		remaining = update_testset(i)
+		return (remaining < (self.initial_test_size * self.success_percentage))
+
+	def run(self):
+		with open(os.path.join(self.output_dir, 'successes'), 'w') as fout:
+			csv.writer(fout).writerow(['c', 'po', 'll'] + map(lambda i: 'out' + str(i), range(self.num_translations)))
+		i = 0
+		self.train_and_translate(i, epochs=0)
+		while not self.results_sufficient(i):
+			i += 1
+			self.update_datasets(i)
+			self.train_and_translate(i)
+		with open(os.path.join(self.output_dir, 'failures'), 'w') as fout:
+			csvout = csv.writer(fout)
+			csvout.writerow(['c', 'po', 'll'] + map(lambda i: 'out' + str(i), range(self.num_translations)))
+			with open(os.path.join(self.datasets_path, 'test%d.fail.%d.csv' % (i - 1, args.num_translations)),
+					  'r') as fin:
+				for l in list(csv.reader(fin))[1:]:
+					csvout.writerow(l[1:])
+
+
+if __name__ == "__main__":
+	import argparse
+
+	parser = argparse.ArgumentParser(description="Evaluate NMT as active learner")
+	parser.add_argument('input', type=str, help="input dataset for translation")
+	parser.add_argument('output', type=str, help="output directory")
+	parser.add_argument('-c', '--codenator_config', type=str, default='configs/codenator4active.config',
+						help="codenator configuration file (default: \'%(default)s\')")
+	parser.add_argument('-d', '--dynmt-config', type=str, default='configs/dynmt4active.config',
+						help="dynmt configuration file (default: \'%(default)s\')")
+	parser.add_argument('-k', '--num-translations', type=int, default=5,
+						help="number of translations per entry (default: %(default)s)")
+	parser.add_argument('-p', '--percentage', type=float, default=0.99,
+						help="required percentage (between 0 and 1) of inputs successfully translated before termination (default: %(default)s)", )
+	parser.add_argument('-v', '--validation-size', type=int, default=1000,
+						help="number of samples in validation dataset (default: %(default)s)")
+	parser.add_argument('-i', '--train-size-initial', type=int, default=0,
+						help="initial number of samples in training dataset (default: %(default)s)")
+	parser.add_argument('-n', '--train-size-increment', type=int, default=1000,
+						help="number of samples to add to training dataset at each round (default: %(default)s)")
+	args = parser.parse_args()
+
+	ActiveLearner(input=args.input, output_dir=args.output, codenator_config=args.codenator_config,
+				  dynmt_config=args.dynmt_config, num_translations=args.num_translations,
+				  success_percentage=args.percentage, validation_size=args.validation_size,
+				  train_size_initial=args.train_size_initial, train_size_increment=args.train_size_increment).run()
---
+++ evaluate4active.py
@@ -0,0 +1,231 @@
+import sys
+import os
+import subprocess
+# import parmap
+import time
+import itertools
+import csv
+import ConfigParser
+import utils.convertPostOrderToC as po2c
+from utils import llvmUtil as c2llvm
+
+
+def runCbmc(timeout):
+	with open(os.devnull, 'w') as fnull:
+		s = subprocess.Popen(['cbmc', 'cbmc' + str(os.getpid()) + '.c'], stdout=fnull, stderr=subprocess.STDOUT)
+		poll_period = 0
+		s.poll()
+		while s.returncode is None and timeout > 0:
+			time.sleep(poll_period)
+			timeout -= poll_period
+			poll_period = min(timeout, poll_period + 1)
+			s.poll()
+		if timeout > 0:
+			if s.returncode == 0:
+				return 0  # equivalent
+			if s.returncode == 6:
+				return 1  # parse
+			return 2  # fail
+		else:
+			s.kill()
+			return 3  # timeout
+
+
+varCount = 10
+
+
+def compareProgs((c, out)):
+	with open('cbmc' + str(os.getpid()) + '.c', 'w') as fcbmc:
+		c = filter(lambda x: len(x) > 0, c.strip().split(' '))
+		yi_c = 0
+		for i in range(len(c)):
+			if c[i][0] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '%', '(', ')', '=',
+							   ';']:
+				if c[i] == 'Y':
+					c[i] = c[i] + '_' + str(yi_c) + '_0'
+					yi_c += 1
+				else:
+					c[i] = c[i] + '_0'
+		c = ' '.join(c)
+		out = filter(lambda x: len(x) > 0, out.strip().split(' '))
+		yi_out = 0
+		for i in range(len(out)):
+			if out[i][0] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '%', '(', ')',
+								 '=', ';']:
+				if out[i] == 'Y':
+					out[i] = out[i] + '_' + str(yi_out) + '_1'
+					yi_out += 1
+				else:
+					out[i] = out[i] + '_1'
+		out = ' '.join(out)
+		if yi_c != yi_out:
+			return 2  # fail
+		fcbmc.write('void main() {\n')
+		fcbmc.write('\tint ' + ','.join(map(lambda i: 'Y_' + str(i) + '_0,Y_' + str(i) + '_1', range(yi_c))))
+		for i in range(varCount):
+			fcbmc.write(',X' + str(i) + '_0,X' + str(i) + '_1')
+		fcbmc.write(';\n')
+		for i in range(varCount):
+			fcbmc.write('\t__CPROVER_assume(X' + str(i) + '_0==X' + str(i) + '_1);\n')
+		fcbmc.write('\t' + c + '\n')
+		fcbmc.write('\t' + out + '\n')
+		for i in range(yi_c):
+			fcbmc.write(
+				'\t__CPROVER_assert(Y_' + str(i) + '_0==Y_' + str(i) + '_1,"computations not equivalent (' + str(
+					i) + ')");\n')
+		fcbmc.write('}')
+	ret = runCbmc(60)
+	os.remove('cbmc' + str(os.getpid()) + '.c')
+	return ret
+
+
+def convertPostOrderToC(po):
+	return po2c.parse(po)
+
+
+def convertCToLLVM(c, config):
+	s = [y + ';' for y in filter(lambda x: len(x) > 0, c.split(';'))]
+	return c2llvm.translateToLLVM(s, config, check_success=True,
+								  assignments_counter=sum([str(x).count(' = ') for x in c]))
+
+
+def evaluateProg(i, p, expected, c, po, ll, out, postOrder, convert, llvm, cbmc, config):
+	# print '\r' + p,
+	sys.stdout.flush()
+	if len(filter(lambda x: len(x) > 0, out)) == 0:
+		return (i, c, po, ll, out, 3)  # fail
+	else:
+		if postOrder:
+			if not convert:
+				if expected in out:
+					return (i, c, po, ll, [expected], 0)  # identical
+			# res = parmap.map(convertPostOrderToC, out)
+			res = map(convertPostOrderToC, out)
+			if all(map(lambda x: not x[0], res)):
+				return (i, c, po, ll, [], 2)  # parse
+			else:
+				if convert:
+					out = map(lambda x: x[1].c().strip(), filter(lambda y: y[0], res))
+				else:
+					return (i, c, po, ll, [], 3)  # fail
+		# compare c code
+		config = ConfigParser.ConfigParser()
+		config.read(args.config)
+		if expected in out:
+			return (i, c, po, ll, [expected], 0)  # identical
+		res = map(lambda x: convertCToLLVM(x, config).strip(), out)
+		if not any(res):
+			return (i, c, po, ll, [], 2)  # parse
+		if llvm:
+			if ll in res:
+				return (i, c, po, ll, [expected], 0)  # identical
+		if cbmc:
+			# res = parmap.map(compareProgs, map(lambda x: (c, x), out))
+			res = map(compareProgs, map(lambda x: (expected, x), out))
+			if 0 in res:
+				return (i, c, po, ll, map(lambda j: out[j], filter(lambda i: res[i] == 0, range(len(res)))), 1)  # equivalent
+			else:
+				if 2 in res:
+					return (i, c, po, ll, [], 3)  # fail
+				else:
+					if 1 in res:
+						return (i, c, po, ll, [], 2)  # parse
+					else:
+						return (i, c, po, ll, [], 4)  # timeout
+		return (i, c, po, ll, [], 3)  # fail
+
+
+def evaluate(k, fin, fc, fpo, fll, fout, postOrder, convert, llvm, cbmc, force, config, fs=None, ff=None):
+	nsuccess = 0
+	nfail = 0
+	ins = [l.strip() for l in fin.readlines()]
+	cs = [l.strip() for l in fc.readlines()]
+	pos = [l.strip() for l in fpo.readlines()]
+	lls = [l.strip() for l in fll.readlines()]
+	outs = [map(lambda x: x.strip(), l.strip().split('|||')[0:2]) for l in fout.readlines()]
+	groups = {}
+	for (n, g) in itertools.groupby(outs, lambda x: x[0]):
+		groups[int(n)] = [x[1] for x in g]
+	max_len = max(len(ins), len(lls), len(groups.keys()))
+	ins = ins + [''] * (max_len - len(ins))
+	lls = lls + [''] * (max_len - len(lls))
+	for i in filter(lambda x: x not in groups.keys(), range(max_len)):
+		groups[i] = []
+	results = map(
+		lambda i: evaluateProg(i, str(i + 1).zfill(len(str(max_len))) + '/' + str(max_len), ins[i], cs[i], pos[i],
+							   lls[i], groups[i], postOrder, convert, llvm, cbmc, config), range(len(ins)))
+	# print ''
+	for x in results:
+		if x[4] in [0, 1]:
+			if fs:
+				fs.writerow([str(x[0]), x[1], x[2], x[3]] + x[4])
+			nsuccess += 1
+		else:
+			if ff:
+				ff.writerow([str(x[0]), x[1], x[2], x[3]] + x[4])
+			nfail += 1
+	if force:
+		if cbmc:
+			for f in os.listdir('.'):
+				if f.startswith('cbmc') and f.endswith('.c'):
+					os.remove(f)
+		if llvm:
+			for f in os.listdir('.'):
+				if f.startswith('tmp') and (f.endswith('.c') or f.endswith('ll')):
+					os.remove(f)
+	return (nsuccess, nfail)
+
+
+def main(f, k, ext, postOrder, convert, llvm, cbmc, force, config):
+	with open(f + '.success.' + str(k) + '.csv', 'w') as fsuccess:
+		with open(f + '.fail.' + str(k) + '.csv', 'w') as ffail:
+			csv.writer(fsuccess).writerow(['line', 'c', 'po', 'll'] + map(lambda i: 'out' + str(i), range(k)))
+			csv.writer(ffail).writerow(['line', 'c', 'po', 'll'] + map(lambda i: 'out' + str(i), range(k)))
+			with open(f + '.corpus.c', 'r') as fc:
+				with open(f + '.corpus.' + ext, 'r') as fin:
+					with open(f + '.corpus.po', 'r') as fpo:
+						with open(f + '.corpus.ll', 'r') as fll:
+							with open(f + '.corpus.' + str(k) + '.out', 'r') as fout:
+								(nsuccess, nfail) = evaluate(k, fin, fc, fpo, fll, fout, postOrder, convert, llvm, cbmc,
+															 force, config, csv.writer(fsuccess), csv.writer(ffail))
+	print str(nsuccess) + ' statements translated successfully'
+	print str(nfail) + ' statements failed to translate'
+
+
+if __name__ == "__main__":
+	import argparse
+
+	parser = argparse.ArgumentParser(description="Evaluate dataset translations")
+	parser.add_argument('dataset', type=str, help="dataset to translate")
+	parser.add_argument('num_translations', type=int, help="number of translations in output for each input")
+	post_order = parser.add_argument('-po', '--post-order', dest='po', help="use translations to post order code",
+									 action='count')
+	convert = parser.add_argument('--convert', dest='convert', help="convert post order to c", action='count')
+
+
+	class ForceConvert(argparse._CountAction):
+		def __call__(self, parser, namespace, values, option_string=None):
+			if getattr(namespace, post_order.dest, False):
+				setattr(namespace, convert.dest, 1)
+			argparse._CountAction.__call__(self, parser, namespace, values, option_string)
+
+
+	parser.add_argument('--llvm', dest='llvm', help="compile c to llvm", action=ForceConvert)
+	parser.add_argument('--cbmc', dest='cbmc', help="use cbmc to compare c", action=ForceConvert)
+	parser.add_argument('-f', '--force-cleanup', dest='force', help="force delete all tmp files when finished",
+						action='count')
+	parser.add_argument('-c', '--config', dest='config', type=str, default='configs/codenator.config',
+						help="configuration file used for llvm compilation (default: \'%(default)s\')")
+	args = parser.parse_args()
+
+	main(args.dataset, args.num_translations, 'po' if (args.po and not args.convert) else 'c', args.po, args.convert,
+		 args.llvm, args.cbmc, args.force, args.config)
+	if args.force:
+		if args.cbmc:
+			for f in os.listdir('.'):
+				if f.startswith('cbmc') and f.endswith('.c'):
+					os.remove(f)
+		if args.llvm:
+			for f in os.listdir('.'):
+				if f.startswith('tmp') and (f.endswith('.c') or f.endswith('ll')):
+					os.remove(f)
