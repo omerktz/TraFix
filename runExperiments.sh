91ba47af764142a67ea5d50bf695d413c27256bf
Omer Katz <omerktz@gmail.com>
2017-12-30 16:38:48 +0200

    prepare for multiple experiment batches

--- runExperiments.sh
+++ runExperiments.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+mkdir $2
 for ((i=0;i<$1;i++))
 do
-    ./runExperiment.sh 1000 dataset$i output$i &
+    ./runExperiment.sh 1000 dataset$i $2/output$i &
 done
 sleep 5
 wait
+rm tmp*.c
+rm tmp*.ll
