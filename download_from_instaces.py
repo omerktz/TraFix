84639586b7d16346e4bd8da07fa4af7dbf14c4d1
Omer Katz <omerktz@gmail.com>
2018-05-09 09:11:09 +0300

    bugfix

--- download_from_instaces.py
+++ download_from_instaces.py
@@ -9,7 +9,7 @@ def handle_instance((dns,i)):
 	client = paramiko.SSHClient()
 	client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 	client.connect(dns, username='ubuntu', key_filename='omer1.pem')
-	sin, sout, serr = client.exec_command('cd Codenator; tar -xzf tmp.tar.gz output* log*')
+	sin, sout, serr = client.exec_command('cd Codenator; tar -czf tmp.tar.gz output* log*')
 	sout.channel.recv_exit_status()
 	sftp = client.open_sftp()
 	sftp.chdir('Codenator')
@@ -18,5 +18,6 @@ def handle_instance((dns,i)):
 	sout.channel.recv_exit_status()
 	client.close()
 
+os.makedirs('downloaded_x86')
 dnss = map(lambda y: y['Instances'][0]['PublicDnsName'], filter(lambda x: x['Instances'][0]['Tags'][0]['Value'].startswith('omer-x86'), ec2.describe_instances()['Reservations']))
 parmap.map(handle_instance, map(lambda i: (dnss[i], i), range(len(dnss))))
