792f02fea2def41443ea128f464028210d334a5b
yuvalol <yuvalolshaker@gmail.com>
2018-11-30 15:20:48 +0200

    order attentions file

--- open_nmt/show_attentions_nicely.py
+++ open_nmt/show_attentions_nicely.py
@@ -9,7 +9,7 @@ def prepare_and_write_line(w,line):
     line = re.sub(r" +", "&", line)
     w.write(line[1:-2] + '\n')
 
-def create_file_of_one_word_by_num(lines, sentence_num, output_path):
+def create_file_of_one_sentence_by_num(lines, sentence_num, output_path):
     sentence_passed = 0
     first_line_of_word = 0
     word_file_path = output_path + 'sentence_num_' + str(sentence_num) + '_attention.txt'
@@ -51,7 +51,7 @@ def from_df_to_image_and_csv(df, word_file_path, whole_df):
     image_output_path = word_file_path.replace('.txt', '.png')
     # print image_output_path
     showAttention(input_sentence, output_sentence, attns, image_output_path)
-    os.system('rm ' + word_file_path)
+
 
 
 
@@ -61,7 +61,7 @@ def make_smaller_file_with_only_important_words(attn_file, output_path, sentence
 
     with open(attn_file) as all_attn:
         lines = all_attn.readlines()
-        word_file_path = create_file_of_one_word_by_num(lines,sentence_num,output_path)
+        word_file_path = create_file_of_one_sentence_by_num(lines, sentence_num, output_path)
 
     if(word_file_path == None):
         return False
@@ -76,7 +76,7 @@ def make_smaller_file_with_only_important_words(attn_file, output_path, sentence
             temp_df = df.loc[df.index.isin(indexes_to_print)]
             from_df_to_image_and_csv(df, word_file_path, False)
             i += num_of_words_each_part
-
+    os.system('rm ' + word_file_path)
     return True
 
 def make_smaller_file_with_only_important_data(attn_file, output_path, num_of_sentences_to_print, only_failed_words, failed_path):
@@ -101,13 +101,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate NMT as active learner")
     parser.add_argument('attn_file', type=str, help="The attn file to read from")
     parser.add_argument('output_path', type=str, help="The place to create out new file")
-    parser.add_argument('-word', type=str, help ="The word that we want to print", default=None)
     parser.add_argument('-sentence_num', type=int, help="The number of word that we want to print", default=2)
-    parser.add_argument('-enlarge_pixels', type=int, help="if we want to make the pixels larger", default=1)
-    parser.add_argument('-num_highest_attentions', type=int, help="how many attentions would we like to see for each word", default=10)
     parser.add_argument('-num_of_sentences_to_print', type=int, help="number of words to print", default=0)
     parser.add_argument('-only_failed_words', type=int, help='will we order attn of only failed results', default=0)
     parser.add_argument('-failed_path', type=str, help='path of failed results', default='')
     parser.add_argument('-iter_num', type=int, help='the number of the itteration', default=0)
     args = parser.parse_args()
-    make_smaller_file_with_only_important_data(args.attn_file, args.num_highest_attentions, args.output_path, args.num_of_sentences_to_print, args.only_failed_words, args.failed_path, args.iter_num)
+    make_smaller_file_with_only_important_data(args.attn_file, args.output_path, args.num_of_sentences_to_print, args.only_failed_words, args.failed_path)
