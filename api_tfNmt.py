d3ba992b48e8b7333f920137bfbec22fdc9f3c00
yuvalol <yuvalolshaker@gmail.com>
2019-01-05 23:37:27 +0200

    use 1 GPU

--- api_tfNmt.py
+++ api_tfNmt.py
@@ -20,7 +20,7 @@ def main(args):
 	# 																								 'data_set_size'] is not None else 300
 
 	train_command = (
-				'python -m ' + train_py + ' --vocab_prefix={0} --train_prefix={1} --dev_prefix={2} --out_dir={3} --num_train_steps={4} --steps_per_stats={5} --num_layers={6} --num_units={7} --metrics={8} --src=ll --tgt=hl --attention=luong --batch_size={9} --max_gradient_norm={10} --optimizer={11} --encoder_type=bi --num_keep_ckpts={12} --learning_rate={13} --steps_per_valid={14} --patience={15} --src_max_len={16} --tgt_max_len={17} --num_gpus=8'.format(
+				'python -m ' + train_py + ' --vocab_prefix={0} --train_prefix={1} --dev_prefix={2} --out_dir={3} --num_train_steps={4} --steps_per_stats={5} --num_layers={6} --num_units={7} --metrics={8} --src=ll --tgt=hl --attention=luong --batch_size={9} --max_gradient_norm={10} --optimizer={11} --encoder_type=bi --num_keep_ckpts={12} --learning_rate={13} --steps_per_valid={14} --patience={15} --src_max_len={16} --tgt_max_len={17}'.format(
 				vocabs, train_dataset, validation_dataset, model_path, num_train_steps, config.getint('TensorFlow', 'eval_after'), config.getint('TensorFlow', 'lstm_layers'), config.getint('TensorFlow', 'rnn_size'), 'bleu', config.getint('TensorFlow', 'batch_size'), config.getint('TensorFlow', 'max_gradient_norm'), config.get('TensorFlow', 'optimizer'), config.getint('TensorFlow', 'models_to_save'), config.getfloat('TensorFlow', 'learning_rate'), config.getint('TensorFlow', 'eval_after'), config.getint('TensorFlow', 'patience'), config.getint('TensorFlow', 'max_len'), config.getint('TensorFlow', 'max_pred')))
 
 	train_command = train_command.strip()
