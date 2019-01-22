f4bde6b7a82187b73804d17c77c068a5451b2552
yuvalol <yuvalolshaker@gmail.com>
2019-01-22 22:51:03 +0200

    run exp with shallow evaluation

--- runExperiment_forConfigs.sh
+++ runExperiment_forConfigs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(python active_learner.py input $1 $3 -v -e -s 3) &> $2
+(python active_learner.py input $1 $3 -v -e -s 3 --use_shallow_evaluation=True) &> $2
 tar -czf $1.tar.gz $1/ $2
