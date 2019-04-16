cf65ceadc87068019847dbe593e2e8fa73591158
Omer Katz <omerktz@gmail.com>
2019-04-16 17:24:18 +0300

    stupid

--- runExperiment.sh
+++ runExperiment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "(python active_learner.py input $1 $3 ${@:4} -v -e) &> $2"
-#tar -czf $1.tar.gz $1/ $2
+(python active_learner.py input $1 $3 ${@:4} -v -e) &> $2
+tar -czf $1.tar.gz $1/ $2
