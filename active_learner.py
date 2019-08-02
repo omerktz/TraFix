3a3cee7b56ceca1dbbcd6a9d7a0af05f1cdd2f98
Omer Katz <omerktz@users.noreply.github.com>
2019-08-02 23:01:25 +0200

    increase train set size

--- active_learner.py
+++ active_learner.py
@@ -337,9 +337,9 @@ if __name__ == "__main__":
 						help="Required percentage (between 0 and 1) of inputs successfully translated before termination (default: %(default)s)", )
 	parser.add_argument('-t', '--validation-size', type=int, default=1000,
 						help="Number of samples in validation dataset (default: %(default)s)")
-	parser.add_argument('-i', '--train-size-initial', type=int, default=10000,
+	parser.add_argument('-i', '--train-size-initial', type=int, default=50000,
 						help="Initial number of samples in training dataset (default: %(default)s)")
-	parser.add_argument('-n', '--train-size-increment', type=int, default=5000,
+	parser.add_argument('-n', '--train-size-increment', type=int, default=10000,
 						help="Number of samples to add to training dataset at each round (default: %(default)s)")
 	parser.add_argument('-x', '--train-set-drop', type=int, default=50,
 						help="Percentage of training dataset to drop at each round (default: %(default)s, value should be between 0 and 100)")
