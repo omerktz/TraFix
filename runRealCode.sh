01b364b14e6209428a7dc71c256e6f3a560cac2d
Omer Katz <omerktz@users.noreply.github.com>
2019-08-16 10:57:10 +0200

    fix real code script

--- runRealCode.sh
+++ runRealCode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source activate tensorflow_p27
 python api_tfnmt.py realcode realcode realcode output$1/datasets/vocabs$2 -m output$1/models/model$2 -c configs/nmt.config --translate -n 5
-python evaluate.py input 5 x86Util.py -v 
+python evaluate.py realcode 5 x86Util.py -v 
